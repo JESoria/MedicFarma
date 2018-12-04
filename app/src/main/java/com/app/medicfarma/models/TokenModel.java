b0c844f0d81869eaea670dd1af43e0879375a2d9
IngSoria <ing_soria@hotmail.com>
2018-12-04 08:21:29 -0600

    Version Inicial 1.0

---
+++ app/src/main/java/com/app/medicfarma/models/TokenModel.java
@@ -0,0 +1,24 @@
+package com.app.medicfarma.models;
+
+public class TokenModel {
+
+    private String outh_name;
+    private String sender_id;
+
+    public String getOuth_name() {
+        return outh_name;
+    }
+
+    public void setOuth_name(String outh_name) {
+        this.outh_name = outh_name;
+    }
+
+    public String getSender_id() {
+        return sender_id;
+    }
+
+    public void setSender_id(String sender_id) {
+        this.sender_id = sender_id;
+    }
+
+}
---
+++ app/src/main/java/com/app/medicfarma/models/UsuarioModel.java
@@ -0,0 +1,103 @@
+package com.app.medicfarma.models;
+
+public class UsuarioModel {
+
+    private int idUsuario;
+    private String usuario;
+    private String password;
+    private String nombres;
+    private String apellidos;
+    private String genero;
+    private String fechaNacimiento;
+    private String correo;
+    private int facebookId;
+
+    public UsuarioModel() {
+
+    }
+
+    public UsuarioModel(int idUsuario, String usuario, String password, String nombres, String apellidos, String genero, String fechaNacimiento, String correo, int facebookId) {
+        this.idUsuario = idUsuario;
+        this.usuario = usuario;
+        this.password = password;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.genero = genero;
+        this.fechaNacimiento = fechaNacimiento;
+        this.correo = correo;
+        this.facebookId = facebookId;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getFacebookId() {
+        return facebookId;
+    }
+
+    public void setFacebookId(int facebookId) {
+        this.facebookId = facebookId;
+    }
+
+}
