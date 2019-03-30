d6ed6b460b3cda9b482536c75f1afe24e84e5fdb
IngSoria <ing_soria@hotmail.com>
2019-03-30 15:43:24 -0600

    nuevas actividades

---
+++ app/src/main/java/com/app/medicfarma/activities/PaymentTypeActivity.java
@@ -0,0 +1,15 @@
+package com.app.medicfarma.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.medicfarma.R;
+
+public class PaymentTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_type);
+    }
+}
